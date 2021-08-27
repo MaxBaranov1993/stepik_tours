c2e5e918b8750dd2dfd6091012ef2ac15da9eb7e
MaxBaranov1993 <77482261+MaxBaranov1993@users.noreply.github.com>
2021-08-27 13:56:50 +0300

    2

--- stepik_tours/urls.py
+++ stepik_tours/urls.py
@@ -1,4 +1,4 @@
-a"""stepik_tours URL Configuration
+"""stepik_tours URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from tours.views import index_view, departure_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', index_view),
+    path('departure/<int:student_id>', departure_view),
 ]
